079f1a8c65104d27ab8de0c289942a05fd7b5f7c
KVRDV <s.kovardaev@gmail.com>
2021-02-22 13:45:12 +0300

    backend ready

--- app.js
+++ app.js
@@ -56,22 +56,44 @@ const capitals = [
   'Z',
 ];
 const symbols = ['!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~'];
-const emtyArray = [];
+const emptyArr = [];
+const passwordProps = [numbers, letters, capitals, symbols, 12];
 
-function getPassword(arr1, arr2, arr3, arr4, passwordLength) {
-  const mixed = arr1.concat(arr2, arr3, arr4);
-  const shuffled = mixed.sort(function () {
+function getPassword([arr1, arr2, arr3, arr4, passwordLength]) {
+  // Объединяем и перемешиваем в один массив все досутпые символы:
+  let mixedArr = arr1.concat(arr2, arr3, arr4).sort(function () {
     return Math.random() - 0.5;
   });
-  shuffled.splice(passwordLength);
-  const password = shuffled.join('');
-  console.log(password);  
-  return password;
+  // Обрезаем до установленной длины:
+  mixedArr.splice(passwordLength);  
+
+  // Делаем проверки на наличие в массиве хотя бы одного элемента, каждого массива:
+  let findNumbers = true;
+  let findLetters = true;
+  let findCapitals = true;
+  let findSymbols = true;
+
+  if (arr1 === numbers) {
+    findNumbers = mixedArr.some((r) => numbers.includes(r));
+  };
+
+  if (arr2 === letters) {
+    findLetters = mixedArr.some((r) => letters.includes(r));
+  };
+
+  if (arr3 === capitals) {
+    findCapitals = mixedArr.some((r) => capitals.includes(r));
+  };
+
+  if (arr4 === symbols) {
+    findSymbols = mixedArr.some((r) => symbols.includes(r));
+  };
+
+  if ((findNumbers === false) || (findLetters === false) || (findCapitals === false) || (findSymbols === false)) {
+    getPassword(passwordProps);
+  } else {
+    return (password = mixedArr.join(''));
+  }
 }
 
-getPassword(numbers, letters, emtyArray, emtyArray, 17);
-
-
-
-
-// alert( animals.indexOf( 'dog' ) != -1 );
+getPassword(passwordProps);
